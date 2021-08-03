46aadb69c8941264fad31236fa932680546faa69
hazel-u <hyeseung0124@gmail.com>
2021-08-04 00:13:00 +0900

    210803 Algorithm2

--- boj/dp/Boj_14002.java
+++ boj/dp/Boj_14002.java
@@ -2,9 +2,9 @@ package boj.dp;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Boj_14002 {
@@ -15,6 +15,7 @@ public class Boj_14002 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int[] A = new int[N];
 		int[] dp = new int[N];
+		int result=1;
 		Arrays.fill(dp, 1);
 		
 		int in=0;
@@ -26,24 +27,23 @@ public class Boj_14002 {
 			for(int j=0; j<i; j++) {
 				if(A[i]>A[j] && dp[i]<dp[j]+1) {
 					dp[i]=dp[j]+1;
+					result = Math.max(dp[i], result);
 				}
 			}
 		}
+		System.out.println(result);
 		
-		int maxDPV=0; // 가장 큰 dp
-		int maxDPI=-1; // 가장 큰 dp값이 있는 곳의 index
-		int maxAV=A[maxDPI];
-		
-		for(int i=0; i<N; i++) {
-			if(dp[i]>maxDPV) {
-				maxDPV=dp[i];
-				maxDPI=i;
+		int cnt=result;
+		Stack<Integer> stack = new Stack<>();
+		for(int i=N-1; i>=0; i--) {
+			if(cnt==dp[i]) {
+				stack.push(A[i]);
+				cnt--;
 			}
 		}
-		System.out.println(maxDPV);
 		
-		for(int i=N; i>0; i--) {
-			if()
+		while(!stack.isEmpty()) {
+			System.out.print(stack.pop()+" ");
 		}
 	}
 }
